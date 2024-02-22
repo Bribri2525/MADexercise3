4c887830d1093546b0c2f46c5110de7b0b778d73
Brianna Rhebok <rhebokbrianna25@gmail.com>
2024-02-22 23:55:40 +0800

    first commit

--- App.tsx
+++ App.tsx
@@ -1,64 +1,131 @@
-import {Text, 
-  View, 
-  Image, 
-  ScrollView, 
-  TextInput, 
-  StyleSheet,
-} from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
-const App = () => {
+const SignInScreen = () => {
+  const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
+  const [useremail, setUseremail] = useState('');
+  const [address, setAddress] = useState('');
+  const [phonenumber, setPhonenumber] = useState('');
+
+  const handleSignIn = () => {
+    // Lakukan proses autentikasi di sini, misalnya dengan mengirimkan data ke backend
+    console.log('Name:', name);
+    console.log('Username:', username);
+    console.log('Email:', useremail);
+    console.log('Address', address);
+    console.log('Phone Number:', phonenumber);
+    // Implementasi autentikasi lebih lanjut di sini
+  };
+
   return (
-    <View>
-      <Text style={style.title}>Top 3 Korean Food</Text>
-      <ScrollView>
-        <TextInput
-        placeholder="Masukkan nama anda"
-        style={style.inputUsername}
-        />
-        <TextInput
-        placeholder="Masukkan makanan favorit anda"
-        style={style.inputUsername}
-        />
-        <Text style={style.words}>Ini adalah 3 makanan korea yang paling populer!</Text>
-        <Image
-          source={require('./assets/food1.jpg')}
-          style={{width: 400,  height: 200}}
-        />
-        <Text style={style.title}> 2 </Text>
-        <Image
-          source={require('./assets/food2.jpg')}
-          style={{width: 400,  height: 200}}
-        />
-        <Text style={style.title}> 3 </Text>
-        <Image
-          source={require('./assets/food3.jpg')}
-          style={{width: 400,  height: 200}}
-        />
-        </ScrollView>
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Registration</Text>
       </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>Name</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Masukkan nama lengkap anda"
+          onChangeText={text => setName(text)}
+          value={name}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>Username</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Masukkan Username anda"
+          onChangeText={text => setUsername(text)}
+          value={username}
+          secureTextEntry={true}
+        />
+        </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>Email</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Masukkan Email anda"
+          onChangeText={text => setUseremail(text)}
+          value={useremail}
+          secureTextEntry={true}
+        />
+        </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>Address</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Masukkan Alamat anda"
+          onChangeText={text => setAddress(text)}
+          value={address}
+          secureTextEntry={true}
+        />
+        </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>Phone Number</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Masukkan Nomor Telfon anda"
+          onChangeText={text => setPhonenumber(text)}
+          value={phonenumber}
+          secureTextEntry={true}
+        />
+      </View>
+      <TouchableOpacity style={styles.button} onPress={handleSignIn}>
+        <Text style={styles.buttonText}>Register</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingHorizontal: 45,
+    paddingTop: 30,
+    backgroundColor: 'white',
+  },
+  titleContainer: {
+    alignSelf: 'flex-start',
+  },
   title: {
-    fontSize: 20,
+    fontSize: 40,
+    marginBottom: 10,
     fontWeight: 'bold',
-    color: 'white',
-    textAlign: 'center',
-    marginTop: 10,
+    color: 'black',
   },
-  words:{
-    fontSize: 15,
-    textAlign: 'center'
+  inputContainer: {
+    width: '100%',
+    marginBottom: 20,
   },
-  inputUsername: {
-    borderColor: 'black',
+  label: {
+    marginBottom: 5,
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  input: {
+    width: '100%',
+    height: 50,
+    borderColor: 'grey',
     borderWidth: 1,
-    margin: 10,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+  },
+  button: {
+    width: '100%',
+    backgroundColor: 'purple',
+    paddingVertical: 12,
     borderRadius: 10,
-    padding: 10,    
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
   },
 });
 
-export default App;
+export default SignInScreen;
